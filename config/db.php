b42bf5574fa73ae58dd462e85f4a4837d0147533
Sushant Khanal <skkhanal45@gmail.com>
2026-05-19 21:52:49 +0545

    Enable SSL for Azure MySQL

--- config/db.php
+++ config/db.php
@@ -1,4 +1,3 @@
-<!-- This is only used for azure host  -->
 <?php
 $host = getenv("DB_HOST");
 $username = getenv("DB_USER");
@@ -6,15 +5,22 @@ $password = getenv("DB_PASSWORD");
 $database = getenv("DB_NAME");
 $port = getenv("DB_PORT") ?: 3306;
 
-$conn = new mysqli(
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+mysqli_real_connect(
+    $conn,
     $host,
     $username,
     $password,
     $database,
-    (int)$port
+    (int)$port,
+    NULL,
+    MYSQLI_CLIENT_SSL
 );
 
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
+if (mysqli_connect_errno()) {
+    die("Database connection failed: " . mysqli_connect_error());
 }
 ?>
