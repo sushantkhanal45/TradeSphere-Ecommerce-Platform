434dd3bc23f69ea327d5f68f831854eecbc08f22
Sushant Khanal <skkhanal45@gmail.com>
2026-05-19 21:22:23 +0545

    Remove localhost DB code from config

--- config/db.php
+++ config/db.php
@@ -1,14 +1,4 @@
-<!-- This code was used for xampp phpMyAdmin connection -->
-<!-- <?php
-$conn = new mysqli("localhost", "root", "", "TradeSphere");
-
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
-?>
- -->
-
- <!-- This is used for azure db connection -->
+<!-- this is used for azure host only -->
 <?php
 $host = "tradesphere-db.mysql.database.azure.com";
 $username = "adminuser@tradesphere-db";
@@ -16,7 +6,13 @@ $password = "#hey it's sk_45..";
 $database = "tradesphere";
 $port = 3306;
 
-$conn = new mysqli($host, $username, $password, $database, $port);
+$conn = new mysqli(
+    $host,
+    $username,
+    $password,
+    $database,
+    $port
+);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
