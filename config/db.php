738a21a7bbfa38d45b467e80c89c61974c751d66
Sushant Khanal <skkhanal45@gmail.com>
2026-05-19 19:47:23 +0545

    azure database setup

--- config/db.php
+++ config/db.php
@@ -1,8 +1,24 @@
-<?php
+<!-- This code was used for xampp phpMyAdmin connection -->
+<!-- <?php
 $conn = new mysqli("localhost", "root", "", "TradeSphere");
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
 ?>
+ -->
 
+ <!-- This is used for azure db connection -->
+  <?php
+$conn = new mysqli(
+    "tradesphere-db.mysql.database.azure.com",
+    "adminuser",
+    "YOUR_AZURE_MYSQL_PASSWORD",
+    "tradesphere",
+    3306
+);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+?>
