c7225a1881eaaf3bfdd57108e04c983768c32a54
Sushant Khanal <skkhanal45@gmail.com>
2026-05-21 15:29:57 +0545

    db problem cleared

--- config/db.php
+++ config/db.php
@@ -1,36 +1,7 @@
 <?php
-$host = getenv("DB_HOST");
-$username = getenv("DB_USER");
-$password = getenv("DB_PASSWORD");
-$database = getenv("DB_NAME");
-$port = getenv("DB_PORT") ?: 3306;
+$conn = new mysqli("localhost", "root", "", "TradeSphere");
 
-$conn = mysqli_init();
-
-mysqli_ssl_set(
-    $conn,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL
-);
-
-mysqli_real_connect(
-    $conn,
-    $host,
-    $username,
-    $password,
-    $database,
-    (int)$port,
-    NULL,
-    MYSQLI_CLIENT_SSL
-);
-
-if (mysqli_connect_errno()) {
-    die("Database connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
 }
 ?>
-
-
-
